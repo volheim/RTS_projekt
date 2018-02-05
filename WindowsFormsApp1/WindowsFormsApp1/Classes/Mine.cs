34d2850e53e0f307867182373e05029c2aea6a9e
Arne <Arne0480@student.eadania.dk>
2018-02-05 10:58:11 +0100

    Class
    Arne

---
+++ WindowsFormsApp1/WindowsFormsApp1/Classes/Mine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Classes
+{
+    class Mine
+    {
+        int health = 100;
+        public void AddResourceToWorker()
+        {
+
+        }
+        public void Position()
+        {
+
+        }
+        public int Health
+        {
+            set
+            {
+                health = value;
+                if (health <= 0)
+                {
+                    
+                }
+            }
+            get
+            {
+                return health;
+            }
+        }
+        public int WorkerCount()
+        {
+            return WorkCounter;
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Classes/YourBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Classes
+{
+    class YourBase
+    {
+
+        public void Collision()
+        {
+
+        }
+        public void Position()
+        {
+        }
+        public void TotalResource()
+        {
+
+        }
+        public void ControlWorkers()
+        {
+
+        }
+        public void BuildNewWorker()
+        {
+
+        }
+        public void UpgradeWorker()
+        {
+
+        }
+        public void BuildPartsToRocket()
+        {
+
+        }
+        public void BuildRocket()
+        {
+
+        }
+    }
+}
